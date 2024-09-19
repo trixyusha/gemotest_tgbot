0131e6ab3b9ebf57ab42020fc5b675a1f18150e0
Ksenia Shanyushkina <79394747+trixyusha@users.noreply.github.com>
2024-09-19 21:53:24 +0300

    Update telemed_keyboard.py

--- app/keyboards_user/inline/telemed_keyboard.py
+++ app/keyboards_user/inline/telemed_keyboard.py
@@ -9,7 +9,6 @@ async def get_telemed_buttons():
     keyboard = InlineKeyboardBuilder()
     telemed_data = await get_telemed_data()
     for item in telemed_data:
-        print(f'[DATA ITEM] TELEMED {item}')
         keyboard.add(InlineKeyboardButton(text = item[1], callback_data = f'telemed-serviceid_{item[0]}', url = item[2]))
     keyboard.add(InlineKeyboardButton(text = '⬅️Назад к категориям', callback_data = f'back_service_categories'))
     keyboard.add(InlineKeyboardButton(text = f'⏪Выйти', callback_data = f'exit_to_research_or_service'))
